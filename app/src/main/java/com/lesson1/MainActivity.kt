bbf53638667056d0f6aea1be9312b40a9aec97a8
yccho <52302743+lazykumasensei@users.noreply.github.com>
2020-11-12 09:00:45 +0900

    add description

--- app/src/main/java/com/lesson1/MainActivity.kt
+++ app/src/main/java/com/lesson1/MainActivity.kt
@@ -1,4 +1,14 @@
 package com.lesson1
+/*
+* Author : youngchil cho
+* Email : chc07ktm@naver.com
+* Description : Android 개발을 위한 기초 레슨1
+* Memo :
+* 1. Activity와 Layout을 연결해 보세요.
+* 2. layout에 id를 할당하고 연결해 보세요
+* 3. kotlin-extensions을 이용해서 개발환경을 편하게 셋팅해 보세요.
+* 4. Button에 클릭 이벤트를 연결해 보세요
+* */
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
